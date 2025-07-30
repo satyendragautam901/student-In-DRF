a80ba87f935e5e09a2d65b27d5c321e294570eb8
satyendragautam90 <satyendragautam901@gmail.com>
2025-07-31 01:00:03 +0530

    add field level and object level validation

--- api/serializers.py
+++ api/serializers.py
@@ -1,4 +1,5 @@
 from rest_framework import serializers
+from rest_framework.serializers import ModelSerializer
 from .models import Student
 
 
@@ -20,3 +21,24 @@ class StudentSerializer(serializers.Serializer):
         instance.save()
 
         return instance
+    
+    def validate_city(self, value): # field level validation
+        if value == "UAE":
+            raise serializers.ValidationError("Foreign student is not accepted right now ")
+
+        return value
+    
+    def validate(self, data):
+        roll = data.get('roll')
+        name = data.get('name')
+
+        if name == "adi" and roll == 3:
+            raise serializers.ValidationError("This is defaulter student")
+        return data
+     
+class StudentModelSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Student
+        fields ='__all__' # this will include all the fields of model eg: name, city, roll
+        # same as fields = ['name', 'city', 'roll']
+        # some property read_only, exclude etc.
